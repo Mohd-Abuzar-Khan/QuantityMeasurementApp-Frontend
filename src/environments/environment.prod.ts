19cc37880aa3219078a7eabcfb9603e294a34f84
Z4RY <abbuzar982@gmail.com>
2026-04-05 23:44:19 +0530

    [Abuzar] : Adding backend configuration for deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+
+  // Live deployment URLs
+  apiUrl: 'https://api-gateway-g587.onrender.com/api/v1',
+  oauth2GoogleUrl: 'https://api-gateway-g587.onrender.com/oauth2/authorization/google',
+  oauth2GithubUrl: 'https://api-gateway-g587.onrender.com/oauth2/authorization/github',
+};
+
